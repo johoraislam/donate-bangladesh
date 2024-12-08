8dd6bad68f660306b60628fd4c2255ee515e5c1f
johoraislam <johoratazthora@gmail.com>
2024-12-08 08:23:41 +0600

    added more html file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const historyContainer = document.getElementById("history-container");
+const cardContainer = document.getElementById("card-container");
+const historyButton = document.getElementById("history-btn");
+const donationButton = document.getElementById("donation-btn");
+const donationHistory = document.getElementById("donation-history");
+const modal = document.getElementById("my_modal_1");
